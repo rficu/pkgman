0d6ca10b8ed9dfa6beec24404c6793369c67e01b
rficu <rficu@airmail.cc>
2021-03-21 16:21:31 +0200

    Implement main interface for pkgman

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,86 @@
-
-use std::env;
+use std::path::{Path, PathBuf};
+use clap::{App, Arg, AppSettings};
 
 mod parser;
+mod network;
+
+fn update() {
+    let home  = std::env::var("HOME").unwrap();
+    let fname = PathBuf::from(format!("{}/.config/pkgman/pkglist.toml", home));
+
+    if !Path::new(&fname).exists() {
+        println!("Config file not found!");
+        return;
+    }
+
+    let path = &fname.into_os_string().into_string().unwrap();
+
+    match network::add(&parser::parseconfig(path).unwrap()) {
+        Ok(_)    => (),
+        Err(err) => println!("Error occurred: {:#?}", err)
+    };
+}
+
+fn add(fname: &str) {
+    match network::add(&parser::parsefile(fname).unwrap()) {
+        Ok(_)    => (),
+        Err(err) => println!("Error occurred: {:#?}", err)
+    };
+}
+
+fn download(name: &str) {
+    match network::download(name) {
+        Ok(_)    => (),
+        Err(err) => println!("Error occurred: {:#?}", err)
+    };
+}
+
+fn query(name: &str) {
+    let pkg_info = match network::query(name) {
+        Ok(info) => info,
+        Err(err) => {
+            println!("Error occurred: {:#?}", err);
+            return ();
+        }
+    };
+
+    println!("{:#?}", pkg_info);
+}
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-    let fname = &args[1];
+    let matches = App::new("pkgman")
+        .about("IPFS-based package manager for Linux")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::with_name("update")
+                 .short("u")
+                 .long("update")
+                 .takes_value(false)
+                 .help("Update all packages"))
+        .arg(Arg::with_name("add")
+                 .short("a")
+                 .long("add")
+                 .takes_value(true)
+                 .help("Add package"))
+        .arg(Arg::with_name("download")
+                 .short("d")
+                 .long("download")
+                 .takes_value(true)
+                 .help("Download package"))
+        .arg(Arg::with_name("query")
+                 .short("q")
+                 .long("query")
+                 .takes_value(true)
+                 .help("Query package"))
+        .get_matches();
 
-    // TODO command line options with explanations
-
-    let pkg = parser::tryparse(&fname);
-    println!("{}", pkg.ipfs_hash)
+    if matches.is_present("update") {
+        update();
+    } else if matches.is_present("add") {
+        add(matches.value_of("add").unwrap());
+    } else if matches.is_present("download") {
+        download(matches.value_of("download").unwrap());
+    } else {
+        query(matches.value_of("query").unwrap());
+    }
 }
