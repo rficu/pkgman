f08fc08cdb33f55c68ca3879d6c4e5c7b1baa82b
rficu <rficu@airmail.cc>
2021-03-24 14:21:28 +0200

    Add ipfs::upload() and ipfs::download() stubs

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -1,10 +1,22 @@
 use serde::{Serialize, Deserialize};
+use ipfs_api::IpfsClient;
+use std::io::Write;
+use futures::TryStreamExt;
+use std::fs::File;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum IPFSError {
     Success,
     Unknown,
     NotFound,
+    AlreadyExists,
     UnableToConnect
 }
 
+pub async fn upload(_fpath: &String) -> IPFSError {
+    return IPFSError::Success;
+}
+
+pub async fn download(name: &String, ipfs_hash: &String) -> IPFSError {
+    return IPFSError::Success;
+}
