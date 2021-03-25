bdfde8826818bb7dbd53b37aeceb1533c404e902
rficu <rficu@airmail.cc>
2021-03-25 04:55:26 +0200

    Implement ipfs::upload()

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -3,6 +3,7 @@ use ipfs_api::IpfsClient;
 use std::io::Write;
 use futures::TryStreamExt;
 use std::fs::File;
+use crate::parser;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum IPFSError {
@@ -10,11 +11,22 @@ pub enum IPFSError {
     Unknown,
     NotFound,
     AlreadyExists,
-    UnableToConnect
+    UnableToConnect,
+    NewerExists
 }
 
-pub async fn upload(_fpath: &String) -> IPFSError {
-    return IPFSError::Success;
+pub async fn upload(pkg: &parser::PkgInfo) -> Result<String, IPFSError> {
+
+    let client = IpfsClient::default();
+    let file = File::open(&pkg.path).unwrap();
+
+    match client.add(file).await {
+        Ok(file) => return Ok(file.hash),
+        Err(err) => {
+            println!("Failed to add file: {:#?}", err);
+            return Err(IPFSError::Unknown);
+        }
+    }
 }
 
 pub async fn download(name: &String, ipfs_hash: &String) -> IPFSError {
